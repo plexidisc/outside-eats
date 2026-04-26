b032be08615d232a503b1af3a4c6eeb4e9994c17
David Gagne <plexidisc@hotmail.com>
2026-04-26 13:10:37 -0700

    Add files via upload

---
+++ shop-data.js
@@ -0,0 +1,342 @@
+var SHOP_DATA = [
+  {
+    "name": "Breville Barista Express",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Barista Express Impress",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Bambino Espresso Machine",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "De'Longhi La Specialista Arte Evo",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Jura E8 Automatic Espresso Machine",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Nespresso Vertuo Next",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Fellow Stagg EKG Electric Kettle",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Baratza Encore ESP Burr Grinder",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "OXO Brew 9-Cup Coffee Maker",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Chemex 8-Cup Pour-Over Coffee Maker",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "KitchenAid Artisan 5-Qt Stand Mixer",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "KitchenAid Professional 600 6-Qt",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "KitchenAid Artisan Mini 3.5-Qt",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cuisinart 5.5-Qt Stand Mixer",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cuisinart 14-Cup Food Processor",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Sous Chef 16-Cup Food Processor",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Ninja Professional Plus Food Processor",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Vitamix Ascent Series A3500",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Lodge 12\" Cast Iron Skillet",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Lodge Cast Iron Grill Pan",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Made In Blue Carbon Steel Pan",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Le Creuset 5.5-Qt Dutch Oven",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "All-Clad D3 10-Piece Cookware Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Staub 4-Qt Braiser",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Mauviel M'Heritage Copper Skillet 10\"",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Demeyere Industry 5-Ply 10\" Skillet",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Made In 10-Piece Cookware Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Caraway Cookware Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Wüsthof Classic 8\" Chef's Knife",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Shun Classic 6-Piece Knife Block Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Wüsthof Gourmet 12-Piece Knife Block",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Global G-2 8\" Chef's Knife",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Mac Professional 8\" Chef's Knife",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Victorinox Fibrox Pro 8\" Chef's Knife",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Boos Maple Cutting Board 18x12\"",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Misen Chef's Knife",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Vitamix 5200 Blender",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Vitamix E310 Explorian Blender",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Super Q Blender",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Instant Pot Duo 7-in-1 8-Qt",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Smart Oven Air Fryer Pro",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cuisinart Air Fryer Toaster Oven",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "KitchenAid Hand Blender",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Joule Sous Vide",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Nordic Ware Baker's Half Sheet (2-pack)",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "USA Pan 9x13 Baking Pan",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Lodge Cast Iron Baking Pan",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Le Creuset Stoneware 9x13 Baker",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "KitchenAid Nonstick 9-Piece Bakeware Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Emile Henry Bread Loaf Baker",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "De Buyer Perforated Baguette Pan",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Wilton 3-Piece Cake Pan Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Coravin Model Two Elite",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Riedel Veritas Cabernet/Merlot Glasses",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Zalto Denk'Art Universal Glass",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Rabbit Wine Aerator & Decanter Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Laguiole En Aubrac Sommelier Knife",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Vinturi Essential Wine Aerator",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "OXO Steel Cocktail Shaker",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cocktail Kingdom Leopold Jigger",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Breville Smoking Gun Pro",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Monin Premium Syrup Variety Pack",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Weber Spirit II E-310 Gas Grill",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Ooni Koda 16 Pizza Oven",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Traeger Pro 575 Pellet Grill",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Big Green Egg Medium",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Lodge Cast Iron Reversible Grill/Griddle",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cuisinart Portable Induction Cooktop",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "ThermoWorks Thermapen ONE",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Yeti Tundra 45 Cooler",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "RTIC Soft Pack 30-Can Cooler",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Coleman Steel-Belted 54-Qt Cooler",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "ORCA 40-Qt Podster Beach Cooler Bag",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Outsunny 10x10 Pop-Up Canopy",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Tommy Bahama 7-Foot Beach Umbrella",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Sport-Brella XL Umbrella with Anchor",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "GCI Outdoor Freestyle Rocker Beach Chair",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "OXO Good Grips 10-Piece POP Container Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Rubbermaid Brilliance 18-Piece Food Storage",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Joseph Joseph DrawerStore Organizer",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cambro 4-Piece Round Container Set",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "OXO Stainless Steel Utensil Holder",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Wüsthof 7-Slot Knife Block",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Boos Mystery Oil & Board Cream",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  },
+  {
+    "name": "Cuisinart Magnetic Knife Block",
+    "photoUrl": "https://m.media-amazon.com/images/I/61xJcNKKLXL.js"
+  }
+];
