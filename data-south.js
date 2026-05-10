d6457b9ebb52c6d25728381918dc482b4396f8d4
David Gagne <plexidisc@hotmail.com>
2026-05-10 11:27:15 -0700

    Edit restaurant: Sana'a CAFE

--- data-south.js
+++ data-south.js
@@ -3005,31 +3005,20 @@ var PREBAKED_DATA_SOUTH = {
     },
     {
       "id": "ChIJhfX8qg6jj4ARoSRAJ4nQhb0",
-      "formattedAddress": "2400 Broadway ste 120, Redwood City, CA 94063, USA",
-      "location": {
-        "latitude": 37.4865934,
-        "longitude": -122.23140699999999
-      },
-      "googleMapsUri": "https://maps.google.com/?cid=13656550732558509217&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQQAhgEIAA",
-      "websiteUri": "https://www.sanaahousecafe.com/",
       "displayName": {
-        "text": "SANA'A CAFE",
+        "text": "Sana'a CAFE",
         "languageCode": "en"
       },
+      "formattedAddress": "2400 Broadway ste 120, Redwood City, CA 94063, USA",
+      "googleMapsUri": "https://maps.google.com/?cid=13656550732558509217&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQQAhgEIAA",
+      "websiteUri": "https://www.sanaahousecafe.com/",
+      "location": {},
       "cuisine": "Middle Eastern",
-      "phone": "(650) 362-3243",
-      "hours": {
-        "Monday": "7:00 AM - 10:00 PM",
-        "Tuesday": "7:00 AM - 10:00 PM",
-        "Wednesday": "7:00 AM - 10:00 PM",
-        "Thursday": "7:00 AM - 10:00 PM",
-        "Friday": "8:00 AM - 12:00 AM",
-        "Saturday": "8:00 AM - 12:00 AM",
-        "Sunday": "7:00 AM - 10:00 PM"
-      },
-      "description": "Authentic Yemeni coffee and specialty drinks made with cardamom, cinnamon, and traditional brewing methods in a cozy, welcoming space.",
       "menuUrl": "https://sanaahousecafe.com/our-menu/",
+      "phone": "(650) 362-3243",
+      "hours": "[object Object]",
       "priceRange": "$$",
+      "description": "Authentic Yemeni coffee and specialty drinks made with cardamom, cinnamon, and traditional brewing methods in a cozy, welcoming space.",
       "photoUrl": "images/restaurant-photos/redwood-city/Sana a Cafe Small.jpeg"
     },
     {
