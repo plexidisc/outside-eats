2f1f5d47add7f0f268bb56393cd95f5ae3515e4c
David Gagne <plexidisc@hotmail.com>
2026-06-29 15:35:34 -0700

    Edit restaurant: Reposado

--- data-central.js
+++ data-central.js
@@ -7337,6 +7337,7 @@ var PREBAKED_DATA_CENTRAL = {
         "fine dining"
       ],
       "menuUrl": "https://www.reposadorestaurant.com/menu",
+      "videoUrl": "https://youtube.com/shorts/VogXO2nsWdI?feature=share",
       "phone": "(650) 292-1321",
       "hours": "Mon-Fri 11:30am-3:00pm, Mon-Thur 4:00pm-10:00pm, Fri-Sat 4:00pm -10:30pm, Sun 4:00pm-9:00pm, Sat-Sun 11:00 am-3:00pm",
       "priceRange": "$$$",
