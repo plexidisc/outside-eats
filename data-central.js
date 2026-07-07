fca5e41ce8fb37cbdb38cd4a8813e38aaa064068
David Gagne <plexidisc@hotmail.com>
2026-07-07 14:57:03 -0700

    Edit restaurant: Kaizen

--- data-central.js
+++ data-central.js
@@ -5370,6 +5370,34 @@ var PREBAKED_DATA_CENTRAL = {
       ]
     },
     {
+      "id": "manual_1783461267620",
+      "displayName": {
+        "text": "Kaizen",
+        "languageCode": "en"
+      },
+      "formattedAddress": "2337 S El Camino Real, San Mateo, CA 94403",
+      "googleMapsUri": "https://www.google.com/maps/place/kaizen+coffee,+san+mateo/data=!4m2!3m1!1s0x808f9ef945b1bf81:0x283ccc9d2ea64e72?sa=X&ved=1t:242&ictx=111",
+      "websiteUri": "http://www.kaizenandcoffee.com/",
+      "location": {
+        "latitude": 37.54531353182516,
+        "longitude": -122.30610293103439
+      },
+      "placeId": "ChIJgb-xRfmej4ARck6mLp3MPCg",
+      "cuisine": "Cafe/Bakery",
+      "keywords": [
+        "espresso",
+        "coffee",
+        "pastries",
+        "latte",
+        "pour over"
+      ],
+      "menuUrl": "https://www.kaizenandcoffee.com/menu",
+      "hours": "Mon-Sun 7am-4pm",
+      "priceRange": "$$",
+      "description": "Very popular coffee spot sourcing coffee from around the world and rotating their selections to bring high quality, crafted coffee experiences.",
+      "outsideEatsRating": 4.9
+    },
+    {
       "id": "ChIJoRzWonufj4ARgwyltSJXaP4",
       "displayName": {
         "text": "Kajiken",
@@ -8954,31 +8982,6 @@ var PREBAKED_DATA_CENTRAL = {
         "fast casual",
         "counter service"
       ]
-    },
-    {
-      "id": "manual_1783461267620",
-      "displayName": {
-        "text": "Kaizen",
-        "languageCode": "en"
-      },
-      "formattedAddress": "2337 S El Camino Real, San Mateo, CA 94403",
-      "googleMapsUri": "https://www.google.com/maps/place/kaizen+coffee,+san+mateo/data=!4m2!3m1!1s0x808f9ef945b1bf81:0x283ccc9d2ea64e72?sa=X&ved=1t:242&ictx=111",
-      "websiteUri": "http://www.kaizenandcoffee.com/",
-      "location": {
-        "latitude": 37.54531353182516,
-        "longitude": -122.30610293103439
-      },
-      "placeId": "ChIJgb-xRfmej4ARck6mLp3MPCg",
-      "cuisine": "Cafe/Bakery",
-      "keywords": [
-        "espresso",
-        "coffee",
-        "pastries"
-      ],
-      "menuUrl": "https://www.kaizenandcoffee.com/menu",
-      "hours": "Mon-Sun 7am-4pm",
-      "priceRange": "$$",
-      "outsideEatsRating": 4.9
     }
   ],
   "Foster City, CA": [
